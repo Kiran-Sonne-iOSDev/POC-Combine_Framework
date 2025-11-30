17e5e2529950299e0109ffafa8aefdc365e075c7
Kiran Sonne <kiransonne04@gmail.com>
2025-11-30 14:22:12 +0530

    Initial Commit

---
+++ POC-Combine_Framework/POC_Combine_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_Combine_FrameworkApp.swift
+//  POC-Combine_Framework
+//
+//  Created by Kiran Sonne on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct POC_Combine_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
